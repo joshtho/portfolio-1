bbf1592b208eb8c458249242190c6ae069dd1d48
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-08-24 14:29:45 -0500

    add stars in css,test image in mainpage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <div id='stars'></div>
+        <div id='stars2'></div>
+        <div id='stars3'></div>
+        <div id='title'>
+        <span>
+        THIS IS
+        </span>
+        <br/>
+        <span>
+        JOSHUA KEANE THOMPSON
+        </span>
+        <br/>
+        <br/>
+        
+        </div>
+      <MainPage />
+    </>
     </div>
   );
 }
---
+++ src/components/MainPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MainPage() {
+  return (
+    <div>
+        <img src='https://img.icons8.com/?size=512&id=U2l6PLAlApUs&format=png' />
+
+    </div>
+  )
+}
+
+export default MainPage
